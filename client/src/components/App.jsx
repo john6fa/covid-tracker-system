bc46cc173edd48a73a62c54be1cdebc9cd9d06c2
John Fa <john6fa@gmail.com>
2021-10-12 09:55:34 -0700

    set up

---
+++ client/src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const axios = require('axios');
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+
+
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const bodyParser = require('body-parser');
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../client/dist'));
+
+// ROUTES
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening at local host: ${PORT}.`);
+})
